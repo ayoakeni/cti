870055aaf206fa2d63d89c59bf3f3ade021459a3
ayo akeni <ayoakeni64@gmail.com>
2024-08-11 13:12:09 +0100

    resize map

--- src/map/GoogleMap.jsx
+++ src/map/GoogleMap.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const GoogleMap = () => {
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <object type="text/html" data="/map.html" width="100%" height="600px"></object>
-    </div>
+    <object type="text/html" data="/map.html" width="100%" height="600px"></object>
   );
 };
 
